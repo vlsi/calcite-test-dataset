e78e16e334b40d5aca1290b3399a0e76dc5cd571
Michael Mior <mmior@uwaterloo.ca>
2016-01-28 09:59:01 -0500

    Update Postgres Puppet module

--- vm/default.pp
+++ vm/default.pp
@@ -74,11 +74,6 @@ node 'ubuntucalcite' {
       user     => 'foodmart',
       password => postgresql_password('foodmart', 'foodmart'),
     }
-    # Create postgresql database
-    postgresql::server::schema {'foodmart':
-      owner    => 'foodmart',
-      db       => 'foodmart',
-    }
 }
 
 node 'ubuntucalcite-not-yet-ready' {
