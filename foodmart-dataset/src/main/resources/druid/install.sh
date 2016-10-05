581fda75817ff3d3a9f9b85a15f632fb288834cb
Jesus Camacho Rodriguez <jcamacho@apache.org>
2016-10-05 07:45:19 +0100

    Update Druid version to 0.9.1.1

--- foodmart-dataset/src/main/resources/druid/install.sh
+++ foodmart-dataset/src/main/resources/druid/install.sh
@@ -32,14 +32,14 @@ echo Install and start Zookeeper
 )
 
 echo Removing previous Druid
-rm -rf druid-0.9.0
+rm -rf druid-0.9.1.1
 
 echo Installing Druid
-if [ ! -f /var/cache/apt/archives/druid-0.9.0-bin.tar.gz ]; then
-  curl --silent http://static.druid.io/artifacts/releases/druid-0.9.0-bin.tar.gz -o /var/cache/apt/archives/druid-0.9.0-bin.tar.gz
+if [ ! -f /var/cache/apt/archives/druid-0.9.1.1-bin.tar.gz ]; then
+  curl --silent http://static.druid.io/artifacts/releases/druid-0.9.1.1-bin.tar.gz -o /var/cache/apt/archives/druid-0.9.1.1-bin.tar.gz
 fi
-tar -xzf /var/cache/apt/archives/druid-0.9.0-bin.tar.gz
-cd druid-0.9.0
+tar -xzf /var/cache/apt/archives/druid-0.9.1.1-bin.tar.gz
+cd druid-0.9.1.1
 bin/init
 
 ln -s /dataset/druid/foodmart .
